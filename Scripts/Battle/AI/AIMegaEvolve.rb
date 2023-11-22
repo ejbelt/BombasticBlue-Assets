88eaa511129b1745ada2f76b88b68340a095f8fd
Eric Belt <ejbelt@mtu.edu>
2023-11-21 22:25:40 -0500

    first commit

---
+++ Scripts/Battle/AI/AIMegaEvolve.rb
@@ -0,0 +1,10 @@
+#===============================================================================
+#
+#===============================================================================
+class Battle::AI
+  # Decide whether the opponent should Mega Evolve.
+  def pbEnemyShouldMegaEvolve?
+    #NO!!!
+    return false
+  end
+end
